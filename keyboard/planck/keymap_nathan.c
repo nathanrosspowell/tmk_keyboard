02483abec498b6528a00b1d0e5ae4906cde6fcbd
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-01-16 01:40:38 -0500

    Add comma, non-nom dot and username macro
    I missed the comma and dot from my symbols layer. The numpad dot was
    moved to match the normal one.
    
    I added a macro to type my username out.
    
    Fixed some documentation.

--- keyboard/planck/keymap_nathan.c
+++ keyboard/planck/keymap_nathan.c
@@ -49,22 +49,24 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * The top row are Visual Studio combos:
     *   'Run', 'Breakpoint', 'Step over', 'Step into', 'Set cursor to line'
     * 2nd row are key combos:
-    *   'ctrl-alt-delete', 'ctrl-shift-escape'
+    *   'ctrl-alt-delete', 'ctrl-shift-escape' 
+    * 3rd row are macros keys:
+    *   'P0' - 'P5' execute a script on Windows machines 
     * ,-----------------------------------------------------------------------.
     * | ESC | F5   | F9 | F10 | F11 |S+F11|CSF10|NLock|Num7 |Num8 |Num9 | Del |
     * |-----------------------------------------------------------------------|
-    * |     |C/A/D|C/S/E| Ins |Print|Pause|SLock|Num. |Num4 |Num5 |Num6 |Num= |
+    * |     |C/A/D|C/S/E| Ins |Print|Pause|SLock|Num0 |Num4 |Num5 |Num6 |Num= |
     * |-----------------------------------------------------------------------|
-    * |     | P0  | P1  | P2  | P3  | P4  | P5  |Num0 |Num1 |Num2 |Num3 |Num/ |
+    * |     | P0  | P1  | P2  | P3  | P4  | P5  |Num. |Num1 |Num2 |Num3 |Num/ |
     * |-----------------------------------------------------------------------|
-    * |     |     |     |     |     |     |     |     |Home |PgDn |PgUp | End |
+    * |     |User |     |     |     |     |     |     |Home |PgDn |PgUp | End |
     * `-----------------------------------------------------------------------'
     */
   [1] = KEYMAP_GRID(
        ESC,  F5,   F9,   F10,  F11,  FN30, FN31, NLCK, P7,   P8,   P9,   DEL,
-       TRNS, FN16, FN17, INS,  PSCR, PAUS, SLCK, PDOT, P4,   P5,   P6,   PEQL,
-       TRNS, FN2,  FN3,  FN4,  FN5,  FN6,  FN7, P0,   P1,   P2,   P3,   PSLS,
-       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, HOME, PGDN, PGUP, END ),
+       TRNS, FN16, FN17, INS,  PSCR, PAUS, SLCK, P0,   P4,   P5,   P6,   PEQL,
+       TRNS, FN2,  FN3,  FN4,  FN5,  FN6,  FN7,  PDOT, P1,   P2,   P3,   PSLS,
+       TRNS, FN8,  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, HOME, PGDN, PGUP, END ),
 
   /* 2: fn right/raise layer
     * ,-----------------------------------------------------------------------.
@@ -72,15 +74,15 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |-----------------------------------------------------------------------|
     * |     | !   | @   | #   | $   | %   | ^   | &   | *   | -   | +   | =   |
     * |-----------------------------------------------------------------------|
-    * |     | _   | '   | "   | `   | ~   |     |     | ]   | )   | }   | >   |
+    * |     | _   | '   | "   | `   | ~   | ,   | .   | ]   | )   | }   | >   |
     * |-----------------------------------------------------------------------|
-    * |     |NextT|PrevT|     |     |     |     |     |Mute |Vol- |Vol+ | P/P |
+    * |     |NextT|PrevT|     |     |     | Esc |     |Mute |Vol- |Vol+ | P/P |
     * `-----------------------------------------------------------------------'
     */
   [2] = KEYMAP_GRID(
        F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,
-       TRNS, FN18, FN19, FN20, FN21, FN22, FN23, FN24, FN25, MINS, FN26,   PAST,
-       TRNS, FN27, QUOT, FN28, GRV,  FN29, TRNS, TRNS, RBRC, FN13, FN14, FN15,
+       TRNS, FN18, FN19, FN20, FN21, FN22, FN23, FN24, FN25, MINS, FN26, PAST,
+       TRNS, FN27, QUOT, FN28, GRV,  FN29, COMM, DOT,  RBRC, FN13, FN14, FN15,
        TRNS, MNXT, MPRV, TRNS, TRNS, TRNS, ESC,  TRNS, MUTE, VOLD, VOLU, MPLY ),
 };
 
@@ -91,6 +93,7 @@ enum macro_id {
     M_P3,
     M_P4,
     M_P5,
+    M_USERNAME
 };
 
 const uint16_t PROGMEM fn_actions[] = {
@@ -103,6 +106,7 @@ const uint16_t PROGMEM fn_actions[] = {
   [5] = ACTION_MACRO(M_P3),
   [6] = ACTION_MACRO(M_P4),
   [7] = ACTION_MACRO(M_P5),
+  [8] = ACTION_MACRO(M_USERNAME),
   // Braces
   [10] = ACTION_MODS_KEY(MOD_LSFT, KC_9), // (
   [11] = ACTION_MODS_KEY(MOD_LSFT, KC_LBRC), // {
@@ -131,6 +135,7 @@ const uint16_t PROGMEM fn_actions[] = {
   [31] = ACTION_MODS_KEY(MOD_LSFT | MOD_LCTL, KC_F10), // Set cursor to line
  };
 
+// Run a script and pass a key number to it. This is Windows specific and the script needs to be on your path.
 // *open run dialog* keypress.py [PRG_NUM]
 #define ADD_PYTHON_PROGRAM_ON_WIN( PRG_NUM )  MACRO( D(LGUI), T(R), U(LGUI), W(100), T(K), T(E), T(Y), T(P), T(R), T(E), T(S), T(S), T(DOT), T(P), T(Y), T(SPC), T(PRG_NUM), END )
 // *return*
@@ -164,6 +169,11 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
             return event.pressed ?
                 ADD_PYTHON_PROGRAM_ON_WIN( 5 ) : 
                 RUN_PYTHON_PROGRAM_ON_WIN;
+        case M_USERNAME:
+            return event.pressed ?
+                MACRO( T(N), T(A), T(T), T(H), T(A), T(N), T(R), T(O), T(S), T(S), T(P), T(O), T(W), T(E), T(L), T(L), END ) :
+                MACRO_NONE;
+
     }
     return MACRO_NONE;
 }
